2430c5b85aa6aef3f7b374d6d62aeb088ba7ae5e
RomanTukhlin <roman.tukhlin@gmail.com>
2024-02-13 13:52:01 +0300

    first

---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,39 @@
+import {createSignal, Component, JSX, createEffect, on, Show, For} from "solid-js";
+import { BsSortDownAlt } from "solid-icons/bs";
+import { BsSortDown } from "solid-icons/bs";
+import styles from "./Search.module.scss";
+
+
+type TProps = {
+  placeholder: string,
+  items: Array<string>
+}
+export const Search: Component<TProps> = (props) => {
+  const [value, setValue] = createSignal('')
+  const [filterItems, setFilterItems] = createSignal(props.items)
+  const [sortVariant, setSortVariant] = createSignal(true)
+
+
+  createEffect(() => {
+    setFilterItems(() => serchFilter(value(), props.items))
+    setFilterItems((current) => sortItems( sortVariant() ,current))
+  })
+  const sortItems = (rev:boolean, items: Array<string>) => {
+    return [...items].sort((a, b) => (rev ? a.localeCompare(b) : b.localeCompare(a)))
+  }
+  const serchFilter = (value: string, items: Array<string>) => {
+    return items.filter((item) => item.toLowerCase().includes(value))
+  }
+  return <div class={styles.container}>
+    <div>
+      <input type="search" class={styles.input} placeholder={props.placeholder} onInput={(e) => setValue(e.target.value.toLowerCase())} value={value()}/>
+      <button type={'button'} class={styles.button} onClick={() => setSortVariant((current) => !current)}>{sortVariant() ?
+          <BsSortDownAlt size={24}/> : <BsSortDown size={24}/>}</button>
+    </div>
+    <div class={styles.list}>
+      <Show when={filterItems()}>
+        <For each={filterItems()}>{item => <div class={styles.item}>{item}</div>}</For>
+      </Show>
+    </div>
+  </div>;
+};
